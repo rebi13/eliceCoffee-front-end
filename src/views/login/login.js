c91b53d8e49bc678dc28166e41f2c7b368659d05
[SW5] 유수호 <soohomode@gmail.com>
2023-07-05 12:25:54 +0900

    feat: 아이디 비밀번호 포커스 기능 추가

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -11,15 +11,17 @@ document.getElementById('submitButton').addEventListener('click', function (even
   const pwdRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
   // 패스워드 : 최소 8자, 하나 이상의 대소문자 및 하나의 숫자, 하나의 특수문자
 
-  // 입력 값이 조건에 맞지 않을 경우(공백) 사용자에게 알려준다.
+  // 입력 값이 조건에 맞지 않을 경우(공백) 사용자에게 알려주고 focus한다.
   if (userId.trim().length === 0) {
     alert('아이디를 입력해주세요.');
-    return;
+    document.querySelector('#idInput').focus();
+    return false;
   }
 
   if (password.trim().length === 0) {
     alert('비밀번호를 입력해주세요.');
-    return;
+    document.querySelector('#passwordInput').focus();
+    return false;
   }
 
   // 엔터키를 눌러도 로그인이 가능하게 한다.
