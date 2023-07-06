c892f9d9b120314dafe189ccb672ffd0bb5cd73b
[SW5] 엄태호 <sisofiy6048@gmail.com>
2023-07-06 15:17:48 +0900

    feat: mypage의 edit 알림 추가
    * 현재 비밀번호 미입력시 알림 처리
    
    * 새로운 비밀번호 미입력시 알림 처리
    
    * 모두 빈값인 경우 알림이 발생하지않도록 처리

--- src/views/mypage/edit/mypage-edit.js
+++ src/views/mypage/edit/mypage-edit.js
@@ -24,19 +24,26 @@ async function fetchNewPassword(currentPassword) {
 }
 
 async function validatePassword(currentPassword, newPassword, confirmNewPassword) {
+    if (!currentPassword) {
+        throw new Error("현재 비밀번호를 입력해주세요.");
+    }
+
+    if (!newPassword) {
+        throw new Error("새로운 비밀번호를 입력해주세요.");
+    }
+
+    // API에서 검증이 필요하기 때문에 추후 삭제해야 할 수도 있음.
     if (newPassword !== confirmNewPassword) {
         throw new Error("비밀번호 확인이 일치하지 않습니다.");
     }
 
-    // 비밀번호가 정규식에 부합하는지
     if (!pwdRegex.test(newPassword)) {
         throw new Error("규칙에 맞지 않는 비밀번호입니다.");
     }
 
-    // fetch
+    // 현재 비밀번호가 맞지 않음 or 비밀번호 변경 성공 처리
     if (!fetchNewPassword(currentPassword)) {
         // API 응답에 따라 조건 처리 필요할 수 있음. Ex) 현재 비밀번호가 맞지 않는 등
-        alert();
         throw new Error("비밀번호 재설정에 실패하였습니다. 다시 시도해주세요.");
     }
 }
@@ -48,10 +55,14 @@ formElem.addEventListener('submit', (event) => {
     const newPassword = event.target.newPassword.value;
     const confirmNewPassword = event.target.confirmNewPassword.value;
 
+    if (!currentPassword && !newPassword) {
+        return;
+    }
+
     validatePassword(currentPassword, newPassword, confirmNewPassword)
     .then(() => {
         alert("비밀번호가 성공적으로 변경되었습니다.");
-        // 리다이렉트 처리 필요할 수 있음.
+        // 리다이렉트 처리 필요
     })
     .catch((err) => {
         alert(err.message);
