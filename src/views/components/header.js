be989204f4d745ff5e130904b3f818276a99419a
[SW5] 유수호 <soohomode@gmail.com>
2023-07-06 14:49:21 +0900

    feat: 헤더푸터내브 css를 common/common.css에 작성

--- src/views/components/header.js
+++ src/views/components/header.js
@@ -1,12 +1,5 @@
 const header = `
-    <header style="
-        display: flex; 
-        justify-content: space-between;
-        align-items: center;
-        padding: 0 15px;
-        background-color: beige;
-        height: 50px;
-    ">
+    <header>
         <div>MENU</div>
         <h1>Elice Coffee</h1>
         <div>LOGIN</div>
--- src/views/components/nav.js
+++ src/views/components/nav.js
@@ -1,13 +1,5 @@
 const nav = `
-    <nav style="
-        display: flex; 
-        justify-content: space-between;
-        align-items: center;
-        padding: 0 15px;
-        background-color: beige;
-        height: 50px;
-        width: 100%;
-    ">
+    <nav>
         <ul style="
             display: flex; 
             justify-content: 
