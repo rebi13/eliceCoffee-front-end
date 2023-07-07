ac63f870278933b5e05210795858214ae56836b0
[SW5] 엄태호 <sisofiy6048@gmail.com>
2023-07-07 14:58:33 +0900

    test: 모킹데이터 생성

---
+++ src/mock/cart.js
@@ -0,0 +1,27 @@
+/**
+ * {
+ *   id: string,
+ *   cart: [{
+ *     productId: string, 
+ *     productName: string, 
+ *     productQuantity: number, 
+ *     price: number, 
+ *     option: {
+ *       weight: number
+ *     }
+ *   }]
+ * }
+ */
+
+export const cartData = {
+    id: "1234",
+    cart: [{
+        productId: "123456", 
+        productName: "브라질 원두", 
+        productQuantity: 2, 
+        price: 5000, 
+        option: {
+            weight: 300
+        }
+    }]
+}
