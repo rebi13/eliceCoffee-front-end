c68557e057fe5fad6357da2bf5e4aa93d165da7a
[SW5] 엄태호 <sisofiy6048@gmail.com>
2023-07-08 12:15:04 +0900

    feat: mypage list 동적 웹으로 전환
    * mock 폴더에 더미데이터 user 추가
    
    * mypage list 동적 웹 전환 작업 완료

--- src/mock/index.js
+++ src/mock/index.js
@@ -2,11 +2,13 @@ import { product, products } from './products.js';
 import { cartData } from './cart.js';
 import { detailProduct } from './detailProduct.js';
 import { orderData } from './order.js';
+import { userData } from './user.js';
 
 export {
     product,
     products,
     cartData,
     detailProduct,
-    orderData
+    orderData,
+    userData
 };
---
+++ src/mock/user.js
@@ -0,0 +1,21 @@
+/**
+ * {
+ *   id:string, 
+ *   password: string, // 필요 X
+ *   name: string, 
+ *   email: string, 
+ *   phoneNumber: string, 
+ *   address: string, 
+ *   profile: string
+ * }
+ */
+
+
+export const userData = {
+    id: "20231235", 
+    name: "데이비드", 
+    email: "abcd1234@gmail.com", 
+    phoneNumber: "010-1234-5678", 
+    address: "서울특별시 서초구 사당동 1213",
+    profileSrc: "../assets/avater.jpg"
+};
