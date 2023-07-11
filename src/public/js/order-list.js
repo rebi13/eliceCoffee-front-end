24141a6f9aff9a38d71a500d03d63c161a487273
[SW5] 엄태호 <sisofiy6048@gmail.com>
2023-07-12 00:52:51 +0900

    feat: order list 페이지에 API 로직 추가
    * 응답 객체에 맞게 일부 키 변경
    
    * 버튼들의 링크 및 이벤트 로직 일부 수정

--- src/public/js/order-list.js
+++ src/public/js/order-list.js
@@ -1,35 +1,38 @@
-import { orderData } from '../mock/order.js';
 import { makeTemplate } from "./common/template.js";
-import { deliveryStatus } from '../constants/index.js';
+import { deliveryStatus, API_END_POINT } from '../constants/index.js';
 import g from './common/common.js';
 
 /* 렌더링 로직 */
 const body = document.querySelector("body");
 
-function renderProduct() {
+function renderProduct(item) {
+    const { name, option, count, price, mainImage } = item;
+
     return `
         <tr>
             <td>
-                <img src="../../../assets/thumbnail/brazil-cerrado.jpg" alt="제품사진" />
+                <img src="${mainImage}" alt="제품사진" />
             </td>
             <td class="product-info">
-                <a href="#">나이키</a>
-                <p>[ 옵션: 200g ]</p>
+                <a href="#">${name}</a>
+                <p>[ 옵션: ${option.weight}g ]</p>
             </td>
-            <td>3</td>
-            <td>150원</td>
+            <td>${count}</td>
+            <td>${price.toLocaleString('en')}원</td>
         </tr>
     `;
 }
 
 function renderOrder(orderData) {
-    const { orderId, items, status, itemTotal, orderDate } = orderData;
-    const totalPrice = itemTotal.toLocaleString();
-    const displayDate = g.formatDate(orderDate);
+    const { id, items, status, itemTotal, createdAt } = orderData;
+    
+    const totalPrice = itemTotal.toLocaleString('en');
+    const displayDate = g.formatDate(createdAt);
+    const canOrderChange = validateCancel("status") ? "" : "disabled";
 
     return `
         <div class="block">
-            <h4 class="widget-title">${displayDate} (${orderId})</h4>
+            <h4 class="widget-title">${displayDate} (${id})</h4>
             <p class="status">${deliveryStatus[status]}</p>
             <table class="table">
                 <colgroup>
@@ -53,120 +56,27 @@ function renderOrder(orderData) {
             <div class="footer text-right">
                 <p class="total-price">총액 : ${totalPrice}원</p>
                 <div>
-                    <a href="order.html" class="btn btn-default">주문수정</a>
-                    <button class="btn btn-default cancelBtn" disabled>주문취소</button>
+                    <a 
+                        href="order/edit/${id}"
+                        class="btn btn-default"
+                        ${canOrderChange}
+                    >
+                        주문수정
+                    </a>
+                    <button 
+                        class="btn btn-default cancelBtn" 
+                        data-status="${status}"
+                        ${canOrderChange}
+                    >
+                        주문취소
+                    </button>
                 </div>
             </div>
         </div>
     `;
 }
 
-{/* <table>
-        <colgroup>
-            <col style="width:27px">        <!--체크박스-->
-            <col style="width:100px">       <!--이미지-->
-            <col style="width:auto">        <!--상품정보-->
-            <col style="width:98px">        <!--판매가-->
-            <col style="width:75px">        <!--수량-->
-            <col style="width:85px">        <!--배송비-->
-            <col style="width:110px">       <!--합계-->
-        </colgroup>
-        <thead class="cart-product-header">
-            <tr>
-                <th scope="col"><input id="select-all-header" type="checkbox"></th>
-                <th scope="col">이미지</th>
-                <th scope="col">상품정보</th>
-                <th scope="col">판매가</th>
-                <th scope="col">수량</th>
-                <th scope="col">배송비</th>
-                <th scope="col">합계</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- 장바구니 리스트 시작 -->
-        <tr>
-                <td>
-                    <input type="checkbox" class="product-checkbox-input">
-                </td>
-                <td>
-                    <a href="#">
-                        <img class="cart-product-image" src="../../assets/thumbnail/brazil-cerrado.jpg" alt="브라질 세라도">
-                    </a>
-                </td>
-                <td class="cart-product-info">
-                    <a href="#">Brazil Cerrado</a>
-                    <p>[ 옵션: 200g ]</p>
-                </td>     
-                <td>3,400원</td>
-                <td>
-                    <div class="cart-count">
-                        <input type="text" size="1" value="1" id="cart-count-input">
-                        <span class="cart-count-button">
-                            <img src="	https://img.echosting.cafe24.com/design/skin/default/product/btn_count_up.gif" alt="수량증가">
-                            <img src="https://img.echosting.cafe24.com/design/skin/default/product/btn_count_down.gif" alt="수량감소">
-                        </span>
-                    </div>
-                </td>
-                <td>3,000원</td>
-                <td id="product-amount">3,400원</td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="checkbox" class="product-checkbox-input">
-                </td>
-                <td>
-                    <a href="#">
-                        <img class="cart-product-image" src="../../assets/thumbnail/brazil-santos.jpg" alt="브라질 세라도">
-                    </a>
-                </td>
-                <td class="cart-product-info">
-                    <a href="#">Brazil Santos</a>
-                    <p>[ 옵션: 300g ]</p>
-                </td>     
-                <td>3,200원</td>
-                <td>
-                    <div class="cart-count">
-                        <input type="text" size="1" value="1" id="cart-count-input">
-                        <span class="cart-count-button">
-                            <img src="	https://img.echosting.cafe24.com/design/skin/default/product/btn_count_up.gif" alt="수량증가">
-                            <img src="https://img.echosting.cafe24.com/design/skin/default/product/btn_count_down.gif" alt="수량감소">
-                        </span>
-                    </div>
-                </td>
-                <td>3,000원</td>
-                <td id="product-amount">3,200원</td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="checkbox" class="product-checkbox-input">
-                </td>
-                <td>
-                    <a href="#">
-                        <img class="cart-product-image" src="../../assets/thumbnail/colombia-supremo.jpg" alt="브라질 세라도">
-                    </a>
-                </td>
-                <td class="cart-product-info">
-                    <a href="#">colombia-supremo</a>
-                    <p>[ 옵션: 400g ]</p>
-                </td>     
-                <td>5,500원</td>
-                <td>
-                    <div class="cart-count">
-                        <input type="text" size="1" value="3" id="cart-count-input">
-                        <span class="cart-count-button">
-                            <img src="	https://img.echosting.cafe24.com/design/skin/default/product/btn_count_up.gif" alt="수량증가">
-                            <img src="https://img.echosting.cafe24.com/design/skin/default/product/btn_count_down.gif" alt="수량감소">
-                        </span>
-                    </div>
-                </td>
-                <td>3,000원</td>
-                <td id="product-amount">16,500원</td>
-            </tr>
-        </tbody>
-    </table> 
-*/}
-
-function render(orderData) {
+function render(orders) {
     return `
         <main>
             <section class="page-header">
@@ -190,7 +100,7 @@ function render(orderData) {
                         <div class="col-md-12">
                             <div class="dashboard-wrapper user-dashboard">
                                 <div class="table-responsive">
-                                    ${renderOrder(orderData)}
+                                    ${orders.map(order => renderOrder(order)).join("")}
                                 </div>
                             </div>
                         </div>
@@ -201,9 +111,27 @@ function render(orderData) {
     `;
 }
 
-makeTemplate(body, render(orderData));
-
 /* 일반 함수 */
+async function getOrderList() {
+    try {
+        const result = await fetch(`${API_END_POINT}/orders`, { credentials: "include" }).then(res => res.json());
+
+        if (result.error !== null) {
+            throw new Error(result.error);
+        }
+        
+        makeTemplate(body, render(result.data));
+
+        const cancelBtns = document.querySelectorAll('.cancelBtn');
+        cancelBtns.forEach((btn) => btn.addEventListener('click', cancelOrder));
+    } catch(err) {
+        console.error(err);
+        alert("데이터를 받아오던 중 에러가 발생했습니다.");
+    }
+}
+
+getOrderList();
+
 function validateCancel(status) {
     const isCancelable = !(
         deliveryStatus[status] === "배송중" || 
@@ -213,32 +141,21 @@ function validateCancel(status) {
     return isCancelable;
 }
 
-function cancelOrder() {
-    // 데모
-    const product = {
-        id: 1,
-        status: "shipping"
-    }
+function cancelOrder(event) {
+    const btn = event.target.closest(".cancelBtn");
+    const orderStatus = btn.dataset.status;
 
     // shipping, deliveryComplete인 경우 alert
-    if (!validateCancel(product.status)) {
+    if (!validateCancel(orderStatus)) {
         alert("이미 배송이 시작되어 주문 취소가 불가합니다.");
         return;
     }
 
     // 배송준비중
-    if (deliveryStatus[product.status] === "배송준비중") {
+    if (deliveryStatus[orderStatus] === "배송준비중") {
         alert("상품을 준비중이어서 관리자의 승인 여부에 따라 취소가 불가할 수 있습니다.");
         return;
     }
 
     alert("주문이 취소되었습니다.");
 }
-
-// 테스트용
-const cancelBtns = document.querySelectorAll('.cancelBtn');
-cancelBtns.forEach((btn) => btn.addEventListener('click', cancelOrder));
-
-// 테스트 코드
-// console.log(validateCancel("shipping")); // false
-// console.log(validateCancel("preparing")); // true
