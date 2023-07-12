c3df049bde54340a955213308ff58e5cbb7811ff
[SW5] 유수호 <soohomode@gmail.com>
2023-07-12 11:51:35 +0900

    fix: nav 하위카테고리 제거, home 하부 커피관련문구 재생

--- src/public/js/components/nav.js
+++ src/public/js/components/nav.js
@@ -3,11 +3,6 @@ const nav = `
         <ul>
             <li class="dropdown">
                 <a class="dropdown-toggle" href="#">국가별</a>
-                <ul class="dropdown-menu">
-                    <li><a href="#">아프리카</a></li>
-                    <li><a href="#">중남미</a></li>
-                    <li><a href="#">아시아</a></li>
-                </ul>
             </li>
 
             <li class="dropdown">
@@ -20,12 +15,7 @@ const nav = `
 
             <li class="dropdown">
                 <a class="dropdown-toggle" href="#">커피용품</a>
-                <ul class="dropdown-menu">
-                    <li><a href="#">드리퍼</a></li>
-                    <li><a href="#">서버</a></li>
-                    <li><a href="#">필터</a></li>
-                    <li><a href="#">그라인더</a></li>
-                </ul>
+                
             </li>
         </ul>
     </nav>
@@ -35,40 +25,55 @@ export { nav };
 
 // 카테고리(국가별, 커피용품)이벤트 리스트를 토글합니다.
 window.addEventListener('DOMContentLoaded', function () {
-    const dropdownToggleList = document.querySelectorAll('.dropdown-toggle');
-    const dropdownMenuList = document.querySelectorAll('.dropdown-menu');
-  
-    dropdownToggleList.forEach((toggle, index) => {
-      const parentLi = toggle.parentElement;
-  
-      parentLi.addEventListener('mouseover', function () {
-        showDropdownMenu(index);
-      });
-  
-      parentLi.addEventListener('mouseout', function (event) {
-        hideDropdownMenu(event, index);
-      });
-  
-      dropdownMenuList[index].addEventListener('mouseover', function () {
-        showDropdownMenu(index);
-      });
-  
-      dropdownMenuList[index].addEventListener('mouseout', function (event) {
-        hideDropdownMenu(event, index);
-      });
+  const dropdownToggleList = document.querySelectorAll('.dropdown-toggle');
+  const dropdownMenuList = document.querySelectorAll('.dropdown-menu');
+
+  dropdownToggleList.forEach((toggle, index) => {
+    const parentLi = toggle.parentElement;
+
+    parentLi.addEventListener('mouseover', function () {
+      showDropdownMenu(index);
     });
-  
-    function showDropdownMenu(index) {
-      const parentLi = dropdownToggleList[index].parentElement;
-      parentLi.classList.add('active');
-    }
-  
-    function hideDropdownMenu(event, index) {
-      const relatedTarget = event.relatedTarget || event.toElement;
-      const parentLi = dropdownToggleList[index].parentElement;
-  
-      if (!parentLi.contains(relatedTarget)) {
-        parentLi.classList.remove('active');
-      }
-    }
+
+    parentLi.addEventListener('mouseout', function (event) {
+      hideDropdownMenu(event, index);
+    });
+
+    dropdownMenuList[index].addEventListener('mouseover', function () {
+      showDropdownMenu(index);
+    });
+
+    dropdownMenuList[index].addEventListener('mouseout', function (event) {
+      hideDropdownMenu(event, index);
+    });
   });
+
+  function showDropdownMenu(index) {
+    const parentLi = dropdownToggleList[index].parentElement;
+    parentLi.classList.add('active');
+  }
+
+  function hideDropdownMenu(event, index) {
+    const relatedTarget = event.relatedTarget || event.toElement;
+    const parentLi = dropdownToggleList[index].parentElement;
+
+    if (!parentLi.contains(relatedTarget)) {
+      parentLi.classList.remove('active');
+    }
+  }
+});
+
+{
+  /* <ul class="dropdown-menu">
+                    <li><a href="#">아프리카</a></li>
+                    <li><a href="#">중남미</a></li>
+                    <li><a href="#">아시아</a></li>
+                </ul>
+
+  <ul class="dropdown-menu">
+                    <li><a href="#">드리퍼</a></li>
+                    <li><a href="#">서버</a></li>
+                    <li><a href="#">필터</a></li>
+                    <li><a href="#">그라인더</a></li>
+                </ul> */
+}
--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,4 +1,4 @@
-import { makeTemplate } from "./common/template.js";
+import { makeTemplate } from './common/template.js';
 import g from './common/common.js';
 
 let contentHead = `
@@ -21,7 +21,7 @@ let contentHead = `
 
     <section class="products section">
         <div class="container">
-            <div class="row">         
+            <div class="row">
     `;
 
 let contentTail = `
@@ -59,19 +59,41 @@ let contentTail = `
             </div>
         </div>
     </section>
-    `;
+
+    <hr>
+<section class="page-write">
+	<div class="container">					
+				<div class="content">
+					<h1 class="page-name">About Coffee</h1>
+					<ol class="">
+						<li class="coffee-write">"커피는 시간과 공간을 초월한 편안함의 청자입니다.<br> 그 향기를 느껴보면 일상에 활력을 불어넣어 새로운 시작으로 당신을 인도합니다."</li>
+            <p></p>
+            <h4 class="coffee-write">
+              지금 이곳에서, 커피를 경험하세요!
+            </h4>
+					</ol>
+				</div>
+	</div>
+</section>
+
+<section class="another-products">
+  <div class="container">
+    <div>
+    </div>
+  </div>
+</section>
+`;
 
 // 상품 목록 데이터 받아오기
-const API_URL = "http://localhost:3001/api/v1/products/main";
+const API_URL = 'http://localhost:3001/api/v1/products/main';
 fetch(API_URL)
-    .then( res => res.json())
-    .then( data => {
-        let result = data.data;
+  .then((res) => res.json())
+  .then((data) => {
+    let result = data.data;
 
-        result.forEach( e => {
-
-            // 데이터 수 만큼 목록 생성
-            let contentCenter = `
+    result.forEach((e) => {
+      // 데이터 수 만큼 목록 생성
+      let contentCenter = `
                 <div class="col-md-4">
                     <div class="product-item">
                         <div class="product-thumb">
@@ -102,23 +124,48 @@ fetch(API_URL)
                             <p class="price">{price}</p>
                         </div>
                     </div>
-                </div>
+                </div>                
             `;
-            contentCenter = contentCenter.replaceAll('{name}', e.name);
-            contentCenter = contentCenter.replaceAll('{discription}', e.description);
-            contentCenter = contentCenter.replaceAll('{price}', g.setParseStringAmount(e.price));
-            contentHead += contentCenter;
-            
-        });
+      contentCenter = contentCenter.replaceAll('{name}', e.name);
+      contentCenter = contentCenter.replaceAll('{discription}', e.description);
+      contentCenter = contentCenter.replaceAll('{price}', g.setParseStringAmount(e.price));
+      contentHead += contentCenter;
+    });
 
-        contentHead += contentTail;
+    contentHead += contentTail;
 
     const body = document.querySelector('body');
     makeTemplate(body, contentHead);
-
-    });
+});
 
 // const homeHTML = `
+
+// <hr>
+// <section class="page-write">
+// 	<div class="container">
+// 				<div class="content">
+// 					<h1 class="page-name">About Coffee</h1>
+// 					<ol class="">
+// 						<li class="coffee-write">"커피는 시간과 공간을 초월한 편안함의 청자입니다.<br> 그 향기를 느껴보면 일상에 활력을 불어넣어 새로운 시작으로 당신을 인도합니다."</li>
+//             <p></p>
+//             <h4 class="coffee-write">
+//               지금 이곳에서, 커피를 경험하세요!
+//             </h4>
+// 					</ol>
+// 				</div>
+// 	</div>
+// </section>
+
+// <section class="another-products">
+//   <div class="container">
+//     <div>
+//     </div>
+//   </div>
+// </section>
+// `;
+
+// const body = document.querySelector('body');
+// makeTemplate(body, homeHTML);
 
 // <section class="md-products">
 //         <div class="container">
@@ -290,30 +337,3 @@ fetch(API_URL)
 //             </div>
 //         </div>
 //     </section>
-
-// <hr>
-// <section class="page-write">
-// 	<div class="container">					
-// 				<div class="content">
-// 					<h1 class="page-name">About Coffee</h1>
-// 					<ol class="">
-// 						<li class="coffee-write">"커피는 시간과 공간을 초월한 편안함의 청자입니다.<br> 그 향기를 느껴보면 일상에 활력을 불어넣어 새로운 시작으로 당신을 인도합니다."</li>
-//             <p></p>
-//             <h4 class="coffee-write">
-//               지금 이곳에서, 커피를 경험하세요!
-//             </h4>
-// 					</ol>
-// 				</div>
-// 	</div>
-// </section>
-
-// <section class="another-products">
-//   <div class="container">
-//     <div>
-//     </div>
-//   </div>
-// </section>
-// `;
-
-// const body = document.querySelector("body");
-// makeTemplate(body, homeHTML);
