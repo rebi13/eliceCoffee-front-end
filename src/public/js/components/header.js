872e98a8d4bc98c47da968ce7398fbd4912b0da4
[SW5] 유수호 <soohomode@gmail.com>
2023-07-12 21:36:47 +0900

    fix: nav카테고리 회원가입 삭제

--- src/public/js/components/header.js
+++ src/public/js/components/header.js
@@ -5,7 +5,6 @@ const header = `
     <a href="/cart"><div>장바구니</div></a>
     <a href="/order"><div>주문목록</div></a>
     <a href="/login"><div>로그인</div></a>
-    <a href="/register"><div>회원가입</div></a>
     <div class="search-container">
         <input type="text" placeholder="다양한 커피를 검색해보세요!">
         <button type="button">검색</button>
