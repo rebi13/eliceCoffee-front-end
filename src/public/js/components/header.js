995d573da335b9279db63c05805b1336e8f31208
rebi13 <rebi13@naver.com>
2023-07-27 18:10:13 +0900

    refactor: 헤더 구조 변경 및 코드 리팩토링

--- src/public/js/components/header.js
+++ src/public/js/components/header.js
@@ -1,54 +1,53 @@
-import g from '../common/common.js';
+import g from "../common/common.js";
 
 function logout() {
-    const token = g.getCookie("loginToken");
+  const token = g.getCookie("loginToken");
 
-    if (token) {
-        g.deleteCookie("loginToken");
-        window.alert("로그아웃 되었습니다.");
-        g.redirectUserPage('/');
-    }
+  if (token) {
+    g.deleteCookie("loginToken");
+    window.alert("로그아웃 되었습니다.");
+    g.redirectUserPage("/");
+  }
 }
 
-function setLoginLogoutButton() {
-    const token = g.getCookie("loginToken");
-
-    return `
-        <a
-            id="login-control-btn"
-            href="${token ? "javascript:void(0)" : "/login"}"
-        >
-            <div>${token ? "로그아웃" : "로그인"}</div>
-        </a>
+function header() {
+  const token = g.getCookie("loginToken");
+  let header = `
+        <header>
+            <div class="logo">
+              <a href="/">☕️ Elice Coffee</a>
+            </div>
+            <nav>
+              <ul class="gnb">
+                <li class="dropdown">
+                  <a href="/product?categoryId=country">추천원두</a>
+                </li>
+                <li class="dropdown">
+                  <a href="/product?categoryId=blend">원두커피</a>
+                </li>
+                <li class="dropdown">
+                  <a href="/product?categoryId=drip">드립커피</a>
+                </li>
+                <li class="dropdown">
+                  <a href="/product?categoryId=supplies">커피용품</a>
+                </li>
+              </ul>
+            </nav>
+            <div class="info">
+              <ul class="user-nav">
+              ${
+                token
+                  ? `<li><a href='/mypage' class='btn btn-cart'></a></li>
+                  <li><a href='/cart' class='btn btn-user'></a></li>
+                  <li><a id='login-control-btn' href="/login">로그아웃</a></li>`
+                  : `<li><a id='login-control-btn' href='/login'>로그인</a></li>`
+              }
+                
+              </ul>
+            </div>
+          </header>
     `;
+  return header;
 }
-
-function setLoggedInButtons() {
-    const token = g.getCookie("loginToken");
-
-    const btns = `
-        <a href="/mypage">
-            <div>마이페이지</div>
-        </a>
-        <a href="/cart">
-            <div>장바구니</div>
-        </a>
-        <a href="/order">
-            <div>주문목록</div>
-        </a>
-    `;
-
-    return token ? btns : "";
-}
-
-const header = `
-    <header>
-        <a href="/">
-            <h1>☕️ Elice Coffee</h1>
-        </a>
-        ${setLoggedInButtons()}
-        ${setLoginLogoutButton()}
-    </header>
-`;
 
 export { header, logout };
